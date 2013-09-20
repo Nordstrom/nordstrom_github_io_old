2dd3da400e2bb3bd56018af960c879bdd6a58f15
Jim Vallandingham <vlandham@gmail.com>
2013-09-20 16:30:06 -0700

    subtle backgrounds

--- js/lazy-load-css.js
+++ js/lazy-load-css.js
@@ -30,14 +30,10 @@
 })(jQuery);
 
 var onDocReadyCss = {
+    '#home':                 { 'background-image': 'url(img/navy_blue.png)' },
     '#purpose':                 { 'background-image': 'url(img/triangle-bg.jpg)' },
     '#about_section':              { 'background-image': 'url(img/purpose.jpg)' },
     '#about_section #labsA':    { 'background-image': 'url(img/opacity-black.png)' },
     '#hiring_section':           { 'background-image': 'url(img/purpose3a.jpg)' },
     '#hiring_section-opacity':   { 'background-image': 'url(img/opacity-black.png)' },
-    '#beyond-mktg-hslide-01':   { 'background-image': 'url(img/black-triangle.jpg)' },
-    '#beyond-mktg-hslide-02':   { 'background-image': 'url(img/imstore.jpg)' },
-    '#beyond-mktg-hslide-03':   { 'background-image': 'url(img/product.jpg)' },
-    '#beyond-mktg-hslide-04':   { 'background-image': 'url(img/interactive.jpg)' },
-    '#beyond-mktg-hslide-05':   { 'background-image': 'url(img/people.jpg)' },
 };
